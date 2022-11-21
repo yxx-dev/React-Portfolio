7128cae029337d46be8a50649280d495bc72f628
yxx-dev <hermitlun@gmail.com>
2022-11-20 22:51:16 -0800

    link fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
             <h1 className="left-title"> </h1>
             <article className="right-content">
                 <section className="contact-channel" id="email"><a href="mailto:yxu13@uw.edu">Email</a></section>
-                <section className="contact-channel" id="linkedin"><a href="www.linkedin.com/in/yxx-dev" target="_blank" rel="noopener noreferrer">LinkedIn</a></section>
+                <section className="contact-channel" id="linkedin"><a href="https://www.linkedin.com/in/yxx-dev" target="_blank" rel="noopener noreferrer">LinkedIn</a></section>
                 <section className="contact-channel" id="GitHub"><a href="https://github.com/yxx-dev" target="_blank" rel="noopener noreferrer">GitHub</a></section>
             </article>
         </section>
